b65260d4ff16b23f649fa8b4303b34f1bdbf547a
vladvinskevitch <vvinskevitch@gmail.com>
2026-05-15 12:45:13 +0300

    [SITIONIX-127] - bump version

--- api-rest/target/generated-sources/annotations/com/sitionix/forgeai/mapper/ForgeAiApiMapperImpl.java
+++ api-rest/target/generated-sources/annotations/com/sitionix/forgeai/mapper/ForgeAiApiMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-15T12:18:09+0300",
+    date = "2026-05-15T12:34:54+0300",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.9 (Oracle Corporation)"
 )
 @Component
