358d92a9910666d2a50b18435cd0706f1bfbfa70
vladvinskevitch <vvinskevitch@gmail.com>
2026-05-22 07:54:57 +0300

    [SITIONIX-9] - make reviewer executor log only

--- application/src/main/java/com/sitionix/forgeai/application/agentexecutor/ReviewerAgentExecutor.java
+++ application/src/main/java/com/sitionix/forgeai/application/agentexecutor/ReviewerAgentExecutor.java
@@ -1,6 +1,5 @@
 package com.sitionix.forgeai.application.agentexecutor;
 
-import com.sitionix.forgeai.domain.usecase.CompleteAgentLane;
 import com.sitionix.forgeai.domain.model.ticket.agentticket.ReviewerPayload;
 import com.sitionix.forgeai.domain.model.ticket.lane.ExecuteAgent;
 import com.sitionix.forgeai.domain.model.ticket.lane.ReadyToStartLane;
@@ -11,15 +10,8 @@ import org.springframework.stereotype.Component;
 @Component("reviewerAgentExecutor")
 public class ReviewerAgentExecutor implements ExecuteAgent<ReviewerPayload> {
 
-    private final CompleteAgentLane completeAgentLane;
-
-    public ReviewerAgentExecutor(final CompleteAgentLane completeAgentLane) {
-        this.completeAgentLane = completeAgentLane;
-    }
-
     @Override
     public void executeLane(final ReadyToStartLane lane) {
         log.info("Execute reviewer lane: " + lane.getLaneId());
-        this.completeAgentLane.completeAndPrepareAgents(lane.getLaneId());
     }
 }
